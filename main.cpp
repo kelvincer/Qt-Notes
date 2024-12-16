3052e9a39351cd9d89c4d3770941866e0c95c86e
Kelvin Cervan <kcervanr@gmail.com>
2024-12-16 14:33:07 -0500

    delete files

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/Notes/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/Notes/qml/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
